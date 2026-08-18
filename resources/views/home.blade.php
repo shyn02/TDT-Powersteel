2c7ccfff23b43def2600af388aff229d374bdf7a
ynoToT <laurzajames@gmail.com>
2026-08-18 14:17:07 +0800

    Replace GI pipe with angle bar in hero slideshow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
             <img src="{{ asset('static/images/Sheet Piles Banner.jpg') }}" alt="">
             <img src="{{ asset('static/images/Roofing Banner.png') }}" alt="">
             <img src="{{ asset('static/images/wiremesh banner.jpg') }}" alt="">
-            <img src="{{ asset('static/images/Galvanized-Iron-Pipes.jpg') }}" alt="">
+            <img src="{{ asset('static/images/Angle Bar.png') }}" alt="">
         </div>
         <!-- Decorative circles -->
         <div class="hero-circles" aria-hidden="true">
